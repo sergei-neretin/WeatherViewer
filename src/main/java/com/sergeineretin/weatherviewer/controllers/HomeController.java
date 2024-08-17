322827faad10645cbbb8e1246f0c5e874fa55b4e
Sergei Neretin <sergei.neretin.2001@gmail.com>
2024-08-17 18:40:14 +0700

    feat: added delete logic

--- src/main/java/com/sergeineretin/weatherviewer/controllers/HomeController.java
+++ src/main/java/com/sergeineretin/weatherviewer/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.sergeineretin.weatherviewer.controllers;
 
 import com.sergeineretin.weatherviewer.Utils;
 import com.sergeineretin.weatherviewer.dto.UserDto;
+import com.sergeineretin.weatherviewer.exceptions.DatabaseException;
 import com.sergeineretin.weatherviewer.exceptions.SessionCookieNotFoundException;
 import com.sergeineretin.weatherviewer.exceptions.SessionExpiredException;
 import com.sergeineretin.weatherviewer.service.SessionService;
@@ -52,25 +53,14 @@ public class HomeController extends BaseController {
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         WebContext webContext = Utils.buildWebContext(req, resp, getServletContext());
         try {
-            String sessionId = Utils.getSessionId(req);
-            UserDto user = service.getUserOrDeleteSession(sessionId);
             String id = req.getParameter("id");
             weatherService.deleteLocation(Long.parseLong(id));
-            weatherService.updateTemperatures(user.getLocations());
-            webContext.setVariable("user", user);
-            webContext.setVariable("authorized", true);
-            templateEngine.process("homeSignedIn", webContext, resp.getWriter());
-        } catch (SessionExpiredException e) {
-            Utils.removeSessionIdCookie(resp);
-            webContext.setVariable("sessionExpired", true);
-            webContext.setVariable("authorized", false);
-            templateEngine.process("homeNotSignedIn", webContext, resp.getWriter());
-        } catch (SessionCookieNotFoundException e) {
-            webContext.setVariable("authorized", false);
-            templateEngine.process("homeNotSignedIn", webContext, resp.getWriter());
+            resp.sendRedirect(getServletContext().getContextPath());
+        } catch (DatabaseException e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 }
